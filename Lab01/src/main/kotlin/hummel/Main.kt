a92e429984f397ffdab0736c406dc21e2b0671ba
Hummel009 <hummel.turbamentum@gmail.com>
2023-09-04 17:04:03 +0300

    Update Main.kt

--- Lab01/src/main/kotlin/hummel/Main.kt
+++ Lab01/src/main/kotlin/hummel/Main.kt
@@ -1,29 +1,53 @@
 package hummel
 
-import com.sun.jna.Library
-import com.sun.jna.Native
-import com.sun.jna.WString
+import com.sun.jna.platform.win32.User32
+import com.sun.jna.platform.win32.WinDef
+import com.sun.jna.platform.win32.WinUser
+import java.awt.Toolkit
 
+fun main() {
+	val className = "ClassHummel009"
+	val windowTitle = "Kotlin JNA WINAPI Test"
 
-object HelloWorldWinAPI {
-	@JvmStatic
-	fun main(args: Array<String>) {
-		// Вызов MessageBox для вывода сообщения "Hello, World!"
-		User32.INSTANCE.MessageBoxW(
-			null, WString("Hello, World!"), WString("MessageBox Title"), User32.MB_OK or User32.MB_ICONINFORMATION
-		)
-	}
+	val windowClass = WinUser.WNDCLASSEX()
+	windowClass.hInstance = null
+	windowClass.lpfnWndProc = WinUser.WindowProc { hwnd, uMsg, wParam, lParam ->
+		when (uMsg) {
+			WinUser.WM_DESTROY -> {
+				User32.INSTANCE.PostQuitMessage(0)
+				WinDef.LRESULT(0)
+			}
 
-	// Определение библиотеки User32.dll с помощью JNA
-	interface User32 : Library {
-		fun MessageBoxW(hwnd: Any?, text: WString?, caption: WString?, type: Int): Int
+			WinUser.WM_SESSION_CHANGE -> {
+				WinDef.LRESULT(0)
+			}
 
-		companion object {
-			val INSTANCE: User32 = Native.load(
-				"user32", User32::class.java
-			) as User32
-			const val MB_OK: Int = 0x00000000
-			const val MB_ICONINFORMATION: Int = 0x00000040
+			else -> User32.INSTANCE.DefWindowProc(hwnd, uMsg, wParam, lParam)
 		}
+	}
+	windowClass.lpszClassName = className
+	windowClass.cbSize = windowClass.size()
+
+	User32.INSTANCE.RegisterClassEx(windowClass)
+
+	val screenSize = Toolkit.getDefaultToolkit().screenSize
+	val screenWidth = screenSize.getWidth().toInt()
+	val screenHeight = screenSize.getHeight().toInt()
+	val windowWidth = 400
+	val windowHeight = 300
+	val x = (screenWidth - windowWidth) / 2
+	val y = (screenHeight - windowHeight) / 2
+
+	val hwnd = User32.INSTANCE.CreateWindowEx(
+		0, className, windowTitle, WinUser.WS_OVERLAPPEDWINDOW, x, y, windowWidth, windowHeight, null, null, null, null
+	)
+
+	User32.INSTANCE.ShowWindow(hwnd, WinUser.SW_SHOW)
+	User32.INSTANCE.UpdateWindow(hwnd)
+
+	val msg = WinUser.MSG()
+	while (User32.INSTANCE.GetMessage(msg, null, 0, 0) != 0) {
+		User32.INSTANCE.TranslateMessage(msg)
+		User32.INSTANCE.DispatchMessage(msg)
 	}
 }
