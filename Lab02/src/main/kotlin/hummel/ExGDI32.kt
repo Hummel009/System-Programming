7df1eb36e34e2146f28d87ea09df4996fa4c0d45
Hummel009 <hummel.turbamentum@gmail.com>
2023-09-20 15:20:23 +0300

    Update ExGDI32.kt

--- Lab02/src/main/kotlin/hummel/ExGDI32.kt
+++ Lab02/src/main/kotlin/hummel/ExGDI32.kt
@@ -10,23 +10,5 @@ internal interface ExGDI32 : GDI32 {
 	}
 
 	fun CreateSolidBrush(color: DWORD?): HBRUSH?
-	fun SetTextColor(hdc: HDC?, color: DWORD?): DWORD?
 	fun TextOutA(hdc: HDC?, x: Int, y: Int, lpString: String?, c: Int): Boolean
-
-	fun CreateFontA(
-		cHeight: Int,
-		cWidth: Int,
-		cEscapement: Int,
-		cOrientation: Int,
-		cWeight: Int,
-		bItalic: Int,
-		bUnderline: Int,
-		bStrikeOut: Int,
-		iCharSet: Int,
-		iOutPrecision: Int,
-		iClipPrecision: Int,
-		iQuality: Int,
-		iPitchAndFamily: Int,
-		pszFaceName: String?
-	): HFONT?
 }
