a2f1b065100ebafb11ec959c5d4579661ecc3d95
Hummel009 <hummel.turbamentum@gmail.com>
2023-09-30 23:29:35 +0300

    ok

--- Lab03/CreateAndInjectLib/AppCode.cpp
+++ Lab03/CreateAndInjectLib/AppCode.cpp
@@ -6,18 +6,22 @@ using namespace std;
 
 DWORD GetProcessIdByProcessName(const string &processName)
 {
+    // Создаем снимок процессов
     HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
     PROCESSENTRY32 processEntry;
 
+    // Инициализируем структуру PROCESSENTRY32 перед использованием
     ZeroMemory(&processEntry, sizeof(processEntry));
     processEntry.dwSize = sizeof(processEntry);
 
     DWORD processId = 0;
 
+    // Получаем информацию о первом процессе в снимке
     if (Process32First(hSnapshot, &processEntry))
     {
         do
         {
+            // Проверяем совпадение имени процесса
             if (processName == processEntry.szExeFile)
             {
                 processId = processEntry.th32ProcessID;
@@ -26,37 +30,47 @@ DWORD GetProcessIdByProcessName(const string &processName)
         } while (Process32Next(hSnapshot, &processEntry));
     }
 
+    // Закрываем дескриптор снимка
     CloseHandle(hSnapshot);
     return processId;
 }
 
 int main()
 {
+    // Задаем имя процесса, который мы хотим найти
     const string processName = "Process1.exe";
     DWORD pid = GetProcessIdByProcessName(processName);
 
+    // Проверяем, был ли процесс найден
     if (pid == 0)
     {
         cout << "Process not found." << endl;
         return 1;
     }
 
+    // Открываем процесс с полным доступом
     HANDLE hRemoteProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
 
+    // Проверяем, успешно ли открытие процесса
     if (hRemoteProcess == NULL)
     {
         cout << "Failed to open process." << endl;
         return 1;
     }
 
+    // Получаем адрес функции LoadLibraryA из kernel32.dll
     LPVOID threadFunction = (LPVOID)GetProcAddress(GetModuleHandle("kernel32.dll"), "LoadLibraryA");
     string argument = "Lib.dll";
 
+    // Выделяем память в удаленном процессе для аргумента строки
     LPVOID argumentAddress = VirtualAllocEx(hRemoteProcess, NULL, argument.length() + 1, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+    // Записываем аргумент в память удаленного процесса
     WriteProcessMemory(hRemoteProcess, argumentAddress, argument.c_str(), argument.length() + 1, NULL);
 
+    // Создаем удаленный поток, вызывающий LoadLibraryA с аргументом
     if (CreateRemoteThread(hRemoteProcess, NULL, 0, (LPTHREAD_START_ROUTINE)threadFunction, argumentAddress, 0, NULL))
     {
+        // Ждем некоторое время, чтобы поток имел шанс выполниться
         Sleep(1000);
         cout << "Thread created." << endl;
     }
@@ -65,6 +79,7 @@ int main()
         cout << "Failed to create thread." << endl;
     }
 
+    // Закрываем дескриптор удаленного процесса
     CloseHandle(hRemoteProcess);
     return 0;
 }
--- Lab03/CreateAndInjectLib/LibCode.cpp
+++ Lab03/CreateAndInjectLib/LibCode.cpp
@@ -8,38 +8,51 @@
 
 using namespace std;
 
+// Функция для замены подстроки в памяти процесса
 void replaceFunction(string data, string replacement)
 {
     HANDLE process = GetCurrentProcess();
 
+    // Получаем информацию о системе
     SYSTEM_INFO si;
     GetSystemInfo(&si);
 
+    // Структура для хранения информации о памяти
     MEMORY_BASIC_INFORMATION info;
+
+    // Указатель на текущий адрес в памяти
     char *p = 0;
 
+    // Проходим по всей памяти процесса
     while (p < si.lpMaximumApplicationAddress)
     {
+        // Получаем информацию о текущем блоке памяти
         if (VirtualQueryEx(process, p, &info, sizeof(info)) == sizeof(info))
         {
+            // Проверяем, что блок памяти выделен и доступен для записи
             if (info.State == MEM_COMMIT && info.AllocationProtect == PAGE_READWRITE)
             {
+                // Выделяем буфер для хранения данных текущего блока памяти
                 vector<char> chunk(info.RegionSize);
 
+                // Считываем данные из текущего блока памяти
                 SIZE_T bytesRead;
                 if (ReadProcessMemory(process, p, &chunk[0], info.RegionSize, &bytesRead))
                 {
+                    // Проходим по данным и ищем совпадение с подстрокой 'data'
                     for (size_t i = 0; i < (bytesRead - data.length()); ++i)
                     {
                         if (equal(data.begin(), data.end(), &chunk[i]))
                         {
+                            // Найдено совпадение, заменяем подстроку
                             char *ref = static_cast<char *>(p) + i;
                             copy(replacement.begin(), replacement.end(), ref);
-                            ref[replacement.length()] = 0;
+                            ref[replacement.length()] = 0;  // Добавляем нулевой символ в конец замененной строки
                         }
                     }
                 }
             }
+            // Переходим к следующему блоку памяти
             p += info.RegionSize;
         }
     }
