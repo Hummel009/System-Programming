61bb1f2f8f49ffa29df09309762fb4eebec1dbee
Hummel009 <hummel.turbamentum@gmail.com>
2023-09-30 22:39:21 +0300

    works

--- Lab03/CreateAndInjectLib/AppCode.cpp
+++ Lab03/CreateAndInjectLib/AppCode.cpp
@@ -1,56 +1,70 @@
-#include <stdio.h>
-#include <iostream>
-#include <conio.h>
 #include <windows.h>
 #include <tlhelp32.h>
+#include <iostream>
 
 using namespace std;
 
-DWORD GetProcessIdByProcessName(string processName)
+DWORD GetProcessIdByProcessName(const string &processName)
 {
-	DWORD processId = 0;
-	HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-	PROCESSENTRY32 processEntry;
+    HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    PROCESSENTRY32 processEntry;
 
-	ZeroMemory(&processEntry, sizeof(processEntry));
-	processEntry.dwSize = sizeof(processEntry);
+    ZeroMemory(&processEntry, sizeof(processEntry));
+    processEntry.dwSize = sizeof(processEntry);
 
-	bool isFound = false;
-	while (Process32Next(hSnapshot, &processEntry) && !isFound)
-	{
-		if (!processName.compare(processEntry.szExeFile))
-		{
-			processId = processEntry.th32ProcessID;
-			isFound = true;
-		}
-	}
+    DWORD processId = 0;
 
-	return processId;
+    if (Process32First(hSnapshot, &processEntry))
+    {
+        do
+        {
+            if (processName == processEntry.szExeFile)
+            {
+                processId = processEntry.th32ProcessID;
+                break;
+            }
+        } while (Process32Next(hSnapshot, &processEntry));
+    }
+
+    CloseHandle(hSnapshot);
+    return processId;
 }
 
 int main()
 {
-	DWORD pid = GetProcessIdByProcessName("Process1.exe");
-	cout << "PID: " << pid << endl;
+    const string processName = "Process1.exe";
+    DWORD pid = GetProcessIdByProcessName(processName);
 
-	HANDLE hRemoteProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
+    if (pid == 0)
+    {
+        cout << "Process not found." << endl;
+        return 1;
+    }
 
-	cout << "\nPID finded" << endl;
+    HANDLE hRemoteProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
 
-	LPVOID threadFunction = (LPVOID)GetProcAddress(GetModuleHandle("kernel32.dll"), "LoadLibraryA");
-	string argument("Lib.dll");
+    if (hRemoteProcess == NULL)
+    {
+        cout << "Failed to open process." << endl;
+        return 1;
+    }
 
-	LPVOID argumentAddress = VirtualAllocEx(hRemoteProcess, NULL, argument.length() + 1, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+    LPVOID threadFunction = (LPVOID)GetProcAddress(GetModuleHandle("kernel32.dll"), "LoadLibraryA");
+    string argument = "Lib.dll";
 
-	WriteProcessMemory(hRemoteProcess, (LPVOID)argumentAddress, argument.c_str(), argument.length() + 1, NULL);
+    LPVOID argumentAddress = VirtualAllocEx(hRemoteProcess, NULL, argument.length() + 1, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+    WriteProcessMemory(hRemoteProcess, argumentAddress, argument.c_str(), argument.length() + 1, NULL);
 
-	if (CreateRemoteThread(hRemoteProcess, NULL, 0, (LPTHREAD_START_ROUTINE)threadFunction, (LPVOID)argumentAddress, 0, NULL))
-	{
-		Sleep(1000);
-		cout << "Creating thread" << endl;
-		CloseHandle(hRemoteProcess);
-	}
-	else
-		cout << "Cant create thread" << endl;
-	return 0;
+    if (CreateRemoteThread(hRemoteProcess, NULL, 0, (LPTHREAD_START_ROUTINE)threadFunction, argumentAddress, 0, NULL))
+    {
+        Sleep(1000);
+        cout << "Thread created." << endl;
+    }
+    else
+    {
+        cout << "Failed to create thread." << endl;
+    }
+
+    CloseHandle(hRemoteProcess);
+    return 0;
 }
--- Lab03/CreateAndInjectLib/LibCode.cpp
+++ Lab03/CreateAndInjectLib/LibCode.cpp
@@ -24,17 +24,17 @@ void replaceFunction(string data, string replacement)
         {
             if (info.State == MEM_COMMIT && info.AllocationProtect == PAGE_READWRITE)
             {
-                std::vector<char> chunk(info.RegionSize);
+                vector<char> chunk(info.RegionSize);
 
                 SIZE_T bytesRead;
                 if (ReadProcessMemory(process, p, &chunk[0], info.RegionSize, &bytesRead))
                 {
                     for (size_t i = 0; i < (bytesRead - data.length()); ++i)
                     {
-                        if (std::equal(data.begin(), data.end(), &chunk[i]))
+                        if (equal(data.begin(), data.end(), &chunk[i]))
                         {
                             char *ref = static_cast<char *>(p) + i;
-                            std::copy(replacement.begin(), replacement.end(), ref);
+                            copy(replacement.begin(), replacement.end(), ref);
                             ref[replacement.length()] = 0;
                         }
                     }
