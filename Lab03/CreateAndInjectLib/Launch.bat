9d2e84bcfec4a801c2b445f3b8557b22f8c21da9
Hummel009 <hummel.turbamentum@gmail.com>
2023-09-30 22:31:05 +0300

    1

--- Lab03/CreateAndInjectLib/Launch.bat
+++ Lab03/CreateAndInjectLib/Launch.bat
@@ -1,3 +1,8 @@
+g++ Process.cpp -o Process1.exe
+g++ Process.cpp -o Process2.exe
+start cmd /k "Process1.exe"
+start cmd /k "Process2.exe"
+
 g++ -shared -o Lib.dll LibCode.cpp
 g++ AppCode.cpp -o App.exe
 App
--- Lab03/CreateAndInjectLib/Process.bat
+++
@@ -1,4 +0,0 @@
-g++ Process.cpp -o Process1.exe
-g++ Process.cpp -o Process2.exe
-start cmd /k "Process1.exe"
-start cmd /k "Process2.exe"
