9359c80c420f787297318f4ebeaee4ca5880ef4b
Hummel009 <hummel.turbamentum@gmail.com>
2023-10-21 00:22:00 +0300

    infix lol

--- Lab04/src/WindowsMain/kotlin/Main.kt
+++ Lab04/src/WindowsMain/kotlin/Main.kt
@@ -1,6 +1,8 @@
 import kotlinx.cinterop.*
 import platform.windows.*
 
+val log: MutableMap<String, Int> = mutableMapOf()
+
 fun main() {
 	memScoped {
 		val name = "Hummel009"
@@ -11,35 +13,37 @@ fun main() {
 		val dwFlag = allocArray<DWORDVar>(1)
 		dwFlag[0] = 0u
 
-		val log = mutableMapOf<String, Int>()
-
 		val hKey: HKEYVar = alloc()
 		val data = "AMOGUS"
-		log["Create Key"] = RegCreateKeyExA(
+
+		"Create Key" to RegCreateKeyExA(
 			HKEY_CURRENT_USER, path, 0u, null, REG_OPTION_VOLATILE.toUInt(), KEY_WRITE.toUInt(), null, hKey.ptr, null
 		)
-		log["Set Value"] = RegSetValueExA(hKey.value, name, 0u, REG_SZ.toUInt(), data.ptr(), data.sizeOf())
-		log["Close Key"] = RegCloseKey(hKey.value)
-		log["Get Value"] = RegGetValueA(HKEY_CURRENT_USER, path, name, RRF_RT_REG_SZ.toUInt(), null, szBuf, dwBufLen)
+		"Set Value" to RegSetValueExA(hKey.value, name, 0u, REG_SZ.toUInt(), data.ptr(), data.sizeOf())
+		"Close Key" to RegCloseKey(hKey.value)
+		"Get Value" to RegGetValueA(HKEY_CURRENT_USER, path, name, RRF_RT_REG_SZ.toUInt(), null, szBuf, dwBufLen)
 
 		println(szBuf.toKString())
 
 		val hKeyRe: HKEYVar = alloc()
 		val replace = "SUS"
-		log["Open Key Again"] = RegOpenKeyExA(HKEY_CURRENT_USER, path, 0u, KEY_SET_VALUE.toUInt(), hKeyRe.ptr)
-		log["Set New Value"] = RegSetValueExA(hKeyRe.value, name, 0u, REG_SZ.toUInt(), replace.ptr(), replace.sizeOf())
-		log["Close Key Again"] = RegCloseKey(hKeyRe.value)
-		log["Get New Value"] = RegGetValueA(
-			HKEY_CURRENT_USER, path, name, RRF_RT_REG_SZ.toUInt(), dwFlag, szBuf, dwBufLen
-		)
+
+		"Open Key Again" to RegOpenKeyExA(HKEY_CURRENT_USER, path, 0u, KEY_SET_VALUE.toUInt(), hKeyRe.ptr)
+		"Set New Value" to RegSetValueExA(hKeyRe.value, name, 0u, REG_SZ.toUInt(), replace.ptr(), replace.sizeOf())
+		"Close Key Again" to RegCloseKey(hKeyRe.value)
+		"Get New Value" to RegGetValueA(HKEY_CURRENT_USER, path, name, RRF_RT_REG_SZ.toUInt(), dwFlag, szBuf, dwBufLen)
 
 		println(szBuf.toKString())
 		println("Flag: ${dwFlag[0]}")
 
-		log["Delete Key"] = RegDeleteKeyValueA(HKEY_CURRENT_USER, path, name)
+		"Delete Key" to RegDeleteKeyValueA(HKEY_CURRENT_USER, path, name)
 
 		log.forEach { (key, value) -> println("$key: ${if (value == ERROR_SUCCESS) "OK" else "NOT OK"}") }
 	}
+}
+
+private infix fun String.to(signal: Int) {
+	log[this] = signal
 }
 
 private fun String.sizeOf(): DWORD = encodeToByteArray().toUByteArray().size.toUInt()
