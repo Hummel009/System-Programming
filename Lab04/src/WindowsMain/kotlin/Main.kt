8ad685da2f672c6297c48d74b585914b253259f4
Hummel009 <hummel.turbamentum@gmail.com>
2023-10-20 23:26:39 +0300

    Update Main.kt

--- Lab04/src/WindowsMain/kotlin/Main.kt
+++ Lab04/src/WindowsMain/kotlin/Main.kt
@@ -4,37 +4,33 @@ import platform.windows.*
 fun main() {
 	memScoped {
 		val data = "AMOGUS"
-		val replacement = "SUS"
+		val replace = "SUS"
 		val name = "Hummel009"
 		val path = "Software\\RegistrySample\\"
-
-		val hKey: HKEYVar = alloc()
+		val szBuf = allocArray<CHARVar>(MAX_PATH)
+		val dwBufLen = allocArray<DWORDVar>(1)
+		dwBufLen[0] = MAX_PATH.toUInt()
 
 		val map = mutableMapOf<String, Int>()
 
+		val hKey: HKEYVar = alloc()
 		map["createKey1"] = RegCreateKeyExA(
 			HKEY_CURRENT_USER, path, 0u, null, REG_OPTION_VOLATILE.toUInt(), KEY_WRITE.toUInt(), null, hKey.ptr, null
 		)
 		map["setValue1"] = RegSetValueExA(hKey.value, name, 0u, REG_SZ.toUInt(), data.ptr(), data.sizeOf())
 		map["closeKey1"] = RegCloseKey(hKey.value)
-
-		val szBuf = allocArray<CHARVar>(MAX_PATH)
-		val dwBufLen = allocArray<DWORDVar>(1)
-		dwBufLen[0] = MAX_PATH.toUInt()
-
 		map["getValue1"] = RegGetValueA(
 			HKEY_CURRENT_USER, path, name, RRF_RT_REG_SZ.toUInt(), null, szBuf, dwBufLen
 		)
 
 		println(szBuf.toKString())
 
-		val hKeyReopened: HKEYVar = alloc()
-
-		map["openKey2"] = RegOpenKeyExA(HKEY_CURRENT_USER, path, 0u, REG_SZ.toUInt(), hKeyReopened.ptr)
+		val hKeyRe: HKEYVar = alloc()
+		map["openKey2"] = RegOpenKeyExA(HKEY_CURRENT_USER, path, 0u, REG_SZ.toUInt(), hKeyRe.ptr)
 		map["setValue2"] = RegSetValueExA(
-			hKeyReopened.value, name, 0u, REG_SZ.toUInt(), replacement.ptr(), replacement.sizeOf()
+			hKeyRe.value, name, 0u, REG_SZ.toUInt(), replace.ptr(), replace.sizeOf()
 		)
-		map["closeKey2"] = RegCloseKey(hKeyReopened.value)
+		map["closeKey2"] = RegCloseKey(hKeyRe.value)
 		map["getValue2"] = RegGetValueA(
 			HKEY_CURRENT_USER, path, name, RRF_RT_REG_SZ.toUInt(), null, szBuf, dwBufLen
 		)
