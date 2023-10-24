9e1fd63c539d4b0a8235e82b5dc0b6e687e81df5
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2023-10-24 17:05:32 +0300

    Update Main.kt

--- Course/src/WindowsMain/kotlin/Main.kt
+++ Course/src/WindowsMain/kotlin/Main.kt
@@ -1,84 +1,59 @@
 import kotlinx.cinterop.*
-import platform.posix.FILE
-import platform.posix.fclose
-import platform.posix.fopen
-import platform.posix.fwrite
+import platform.posix.*
 import platform.windows.*
-
-var NUM_BUFFERS: Int = 3
 
 val log: MutableMap<String, String> = mutableMapOf()
 
 fun main() {
 	memScoped {
-		val hWaveIn = alloc<HWAVEINVar>()
-		val waveHeader = allocArray<WAVEHDR>(NUM_BUFFERS)
-
-		val wfx = alloc<WAVEFORMATEX>()
-		wfx.wFormatTag = WAVE_FORMAT_PCM.toUShort() // or appropriate format tag
-		wfx.nChannels = 2u // number of channels (1 for mono, 2 for stereo, etc.)
-		wfx.nSamplesPerSec = 44100u // sample rate (Hz)
-		wfx.nAvgBytesPerSec =
-			(44100 * 2 * 16 / 8).toUInt() // average bytes per second (sample rate * number of channels * bits per sample / 8)
-		wfx.nBlockAlign = (2 * 16 / 8).toUShort() // block align (number of channels * bits per sample / 8)
-		wfx.wBitsPerSample = 16u // bits per sample
-		wfx.cbSize = 0u // size of extra information (0 for PCM)
-
 		val devices = waveInGetNumDevs()
 		println("There are $devices microphones.")
 
+		val hwi = alloc<HWAVEINVar>()
+		val wfx = alloc<WAVEFORMATEX>()
+		wfx.wFormatTag = WAVE_FORMAT_PCM.toUShort()
+		wfx.nChannels = 2u
+		wfx.nSamplesPerSec = 44100u
+		wfx.nAvgBytesPerSec = (44100 * 2 * 16 / 8).toUInt()
+		wfx.nBlockAlign = (2 * 16 / 8).toUShort()
+		wfx.wBitsPerSample = 16u
+		wfx.cbSize = 0u
+
 		"waveInOpen" to waveInOpen(
-			hWaveIn.ptr, WAVE_MAPPER, wfx.ptr, 0u, 0u, CALLBACK_NULL.toUInt()
+			hwi.ptr, WAVE_MAPPER, wfx.ptr, 0u, 0u, CALLBACK_NULL.toUInt()
 		)
 
-		for (i in 0 until NUM_BUFFERS) {
-			"waveInPrepareHeader" to waveInPrepareHeader(
-				hWaveIn.value, waveHeader[i].ptr, sizeOf<WAVEHDR>().toUInt()
-			)
+		val wh = alloc<WAVEHDR>()
+		val bufferSize = 10000000
+		val buffer = allocArray<ShortVar>(bufferSize)
 
-			"waveInAddBuffer" to waveInAddBuffer(
-				hWaveIn.value, waveHeader[i].ptr, sizeOf<WAVEHDR>().toUInt()
-			)
-		}
+		wh.lpData = buffer.reinterpret()
+		wh.dwBufferLength = bufferSize.toUInt()
 
-		"waveInStart" to waveInStart(hWaveIn.value)
+		"waveInPrepareHeader" to waveInPrepareHeader(
+			hwi.value, wh.ptr, sizeOf<WAVEHDR>().toUInt()
+		)
 
-		val wavFile = fopen("output.wav", "w") ?: error("Cannot open output.wav for writing")
+		"waveInAddBuffer" to waveInAddBuffer(
+			hwi.value, wh.ptr, sizeOf<WAVEHDR>().toUInt()
+		)
 
-		writeWaveHeader(wavFile, wfx, NUM_BUFFERS)
+		"waveInStart" to waveInStart(hwi.value)
 
-		for (i in 0 until NUM_BUFFERS) {
-			while ((waveHeader[i].dwFlags and WHDR_DONE.toUInt()) == 0u) {
-				Sleep(5000u)
-			}
-			fwrite(waveHeader[i].lpData, 1u, waveHeader[i].dwBufferLength.convert(), wavFile)
-		}
+		println("START SPEAKING!")
 
-		"waveInStop" to waveInStop(hWaveIn.value)
+		Sleep(5000u)
 
-		fclose(wavFile)
+		"waveInStop" to waveInStop(hwi.value)
 
-		"waveInClose" to waveInClose(hWaveIn.value)
+		"waveInClose" to waveInClose(hwi.value)
 
 		log.forEach { (key, value) -> println("$key: $value") }
+
+		println("Recorded: ${wh.dwBytesRecorded / 1000u} kbytes")
 	}
 }
 
 private infix fun String.to(signal: UInt) {
 	log[this] = if (signal.toInt() == MMSYSERR_NOERROR) "OK" else "ERROR CODE: $signal"
-}
-
-fun writeWaveHeader(file: CPointer<FILE>, wfx: WAVEFORMATEX, numBuffers: Int) {
-	val headerSize = 44
-	val dataSize = NUM_BUFFERS * sizeOf<WAVEHDR>()
-
-	fwrite("RIFF".cstr, 1u, 4u, file)
-	fwrite((headerSize + dataSize).toString().cstr, 1u, 4u, file)
-	fwrite("WAVEfmt ".cstr, 1u, 8u, file)
-	fwrite("16".cstr, 1u, 4u, file)
-
-	fwrite(wfx.ptr, 1u, sizeOf<WAVEFORMATEX>().toULong(), file)
-
-	fwrite("data".cstr, 1u, 4u, file)
-	fwrite(dataSize.toString().cstr, 1u, 4u, file)
 }
