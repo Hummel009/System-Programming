236b402fea41f134feaec30a10f47a1ef0cb4f9a
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2023-10-24 17:50:04 +0300

    WORKS!!!

--- Course/src/WindowsMain/kotlin/Main.kt
+++ Course/src/WindowsMain/kotlin/Main.kt
@@ -1,5 +1,8 @@
 import kotlinx.cinterop.*
-import platform.posix.*
+import platform.posix.FILE
+import platform.posix.fclose
+import platform.posix.fopen
+import platform.posix.fwrite
 import platform.windows.*
 
 val log: MutableMap<String, String> = mutableMapOf()
@@ -19,9 +22,7 @@ fun main() {
 		wfx.wBitsPerSample = 16u
 		wfx.cbSize = 0u
 
-		"waveInOpen" to waveInOpen(
-			hwi.ptr, WAVE_MAPPER, wfx.ptr, 0u, 0u, CALLBACK_NULL.toUInt()
-		)
+		"waveInOpen" to waveInOpen(hwi.ptr, WAVE_MAPPER, wfx.ptr, 0u, 0u, CALLBACK_NULL.toUInt())
 
 		val wh = alloc<WAVEHDR>()
 		val bufferSize = 10000000
@@ -30,23 +31,27 @@ fun main() {
 		wh.lpData = buffer.reinterpret()
 		wh.dwBufferLength = bufferSize.toUInt()
 
-		"waveInPrepareHeader" to waveInPrepareHeader(
-			hwi.value, wh.ptr, sizeOf<WAVEHDR>().toUInt()
-		)
-
-		"waveInAddBuffer" to waveInAddBuffer(
-			hwi.value, wh.ptr, sizeOf<WAVEHDR>().toUInt()
-		)
-
+		"waveInPrepareHeader" to waveInPrepareHeader(hwi.value, wh.ptr, sizeOf<WAVEHDR>().toUInt())
+		"waveInAddBuffer" to waveInAddBuffer(hwi.value, wh.ptr, sizeOf<WAVEHDR>().toUInt())
 		"waveInStart" to waveInStart(hwi.value)
 
-		println("START SPEAKING!")
+		println("Recording started!")
 
 		Sleep(5000u)
 
 		"waveInStop" to waveInStop(hwi.value)
-
 		"waveInClose" to waveInClose(hwi.value)
+
+		val wavFileName = "output.wav"
+		val outputFile = fopen(wavFileName, "w")
+
+		outputFile?.let {
+			writeWavHeader(outputFile, wfx.nChannels, wfx.nSamplesPerSec, wfx.wBitsPerSample, wh.dwBytesRecorded)
+			fwrite(buffer, 1u, wh.dwBytesRecorded.toULong(), outputFile)
+			fclose(outputFile)
+		} ?: {
+			println("Error opening file $wavFileName")
+		}
 
 		log.forEach { (key, value) -> println("$key: $value") }
 
@@ -57,3 +62,29 @@ fun main() {
 private infix fun String.to(signal: UInt) {
 	log[this] = if (signal.toInt() == MMSYSERR_NOERROR) "OK" else "ERROR CODE: $signal"
 }
+
+private fun writeWavHeader(
+	outputStream: CPointer<FILE>, channels: UShort, sampleRate: UInt, bitsPerSample: UShort, dataSize: UInt
+) {
+	fwrite("RIFF".ref(), 1u, 4u, outputStream)
+	fwrite((36u + dataSize).ref(), 1u, 4u, outputStream)
+	fwrite("WAVE".ref(), 1u, 4u, outputStream)
+	fwrite("fmt ".ref(), 1u, 4u, outputStream)
+	fwrite(16.ref(), 1u, 4u, outputStream)
+	fwrite(1.ref(), 1u, 2u, outputStream)
+	fwrite(channels.ref(), 1u, 2u, outputStream)
+	fwrite(sampleRate.ref(), 1u, 4u, outputStream)
+	fwrite((sampleRate * channels * bitsPerSample / 8u).ref(), 1u, 4u, outputStream)
+	fwrite((channels * bitsPerSample / 8u).ref(), 1u, 2u, outputStream)
+	fwrite(bitsPerSample.ref(), 1u, 2u, outputStream)
+	fwrite("data".ref(), 1u, 4u, outputStream)
+	fwrite(dataSize.ref(), 1u, 4u, outputStream)
+}
+
+private fun UShort.ref(): CValuesRef<*> = UShortArray(1) { this }.refTo(0)
+
+private fun Int.ref(): CValuesRef<*> = IntArray(1) { this }.refTo(0)
+
+private fun String.ref(): CValuesRef<*> = cstr.getBytes().refTo(0)
+
+private fun UInt.ref(): CValuesRef<*> = UIntArray(1) { this }.refTo(0)
