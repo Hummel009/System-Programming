f695c7832fd9587ae440639663eb152e5fee1241
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2023-10-30 11:04:22 +0300

    Update App.kt

--- CourseFX/src/main/kotlin/hummel/app/App.kt
+++ CourseFX/src/main/kotlin/hummel/app/App.kt
@@ -2,6 +2,7 @@ package hummel.app
 
 import javafx.application.Application
 import javafx.stage.Stage
+import kotlin.system.exitProcess
 
 class App : Application() {
 	private lateinit var windowSize: WindowSize
@@ -18,5 +19,10 @@ class App : Application() {
 		stage.title = "Hummel009's Media Player"
 		stage.setScene(player.scene)
 		stage.show()
+		stage.setOnCloseRequest {
+			it.consume()
+			stage.hide()
+			exitProcess(0)
+		}
 	}
 }
