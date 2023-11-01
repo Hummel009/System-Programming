4b3fd5d0412f2e90f7e531c7a622e28dbe7bc6b6
Hummel009 <hummel.turbamentum@gmail.com>
2023-11-01 12:23:18 +0300

    Update Task1.kt

--- Lab05/src/main/kotlin/hummel/Task1.kt
+++ Lab05/src/main/kotlin/hummel/Task1.kt
@@ -2,50 +2,20 @@ package hummel
 
 import java.util.concurrent.ConcurrentLinkedQueue
 
-class TaskQueue {
-	private val queue = ConcurrentLinkedQueue<Runnable>()
-
-	fun enqueue(task: Runnable) {
-		queue.add(task)
-	}
-
-	fun dequeue(): Runnable? {
-		return queue.poll()
-	}
-
-	fun isEmpty(): Boolean {
-		return queue.isEmpty()
-	}
-}
-
 fun main() {
-	val taskQueue = TaskQueue()
+	val taskQueue = ConcurrentLinkedQueue<Runnable>()
 
 	val insertThread1 = Thread {
 		for (i in 1..5) {
 			val task = { println("задание $i из потока 1") }
-			taskQueue.enqueue(task)
+			taskQueue.add(task)
 		}
 	}
 
 	val insertThread2 = Thread {
 		for (i in 6..10) {
 			val task = { println("задание $i из потока 2") }
-			taskQueue.enqueue(task)
-		}
-	}
-
-	// Поток для извлечения элементов
-	val removeThread = Thread {
-		while (true) {
-			if (!taskQueue.isEmpty()) {
-				val task = taskQueue.dequeue()
-				task?.let {
-					print("Выполняю ")
-					it.run()
-				}
-			}
-			Thread.sleep(500)
+			taskQueue.add(task)
 		}
 	}
 
@@ -53,6 +23,13 @@ fun main() {
 	insertThread2.start()
 	insertThread1.join()
 	insertThread2.join()
-	removeThread.start()
-	removeThread.join()
+
+	while (!taskQueue.isEmpty()) {
+		val task = taskQueue.poll()
+		task?.let {
+			print("Выполняю ")
+			it.run()
+			Thread.sleep(500)
+		}
+	}
 }
